a25a04bb11e0d8efe12e9df45fe268531f97b0f3
Fabio Carvalho <fabio.asatru@gmail.com>
2017-10-22 12:55:27 -0400

    Inclusão de excluir e publicar

--- teste.js
+++ teste.js
@@ -1,3 +1,5 @@
+/*
+
 var webdriver = require('selenium-webdriver'),
 By = webdriver.By,
 until = webdriver.until;
@@ -7,6 +9,7 @@ var driver = new webdriver.Builder()
 .build();
 
 
+
 console.log('**************************ROBO REDMAXX*************************************');
 driver.get('https://us.qlikcloud.com/hub/personal');
 driver.findElement(By.id("MemberLoginForm_LoginForm_QUsername")).clear();
@@ -14,7 +17,7 @@ driver.findElement(By.id("MemberLoginForm_LoginForm_QUsername")).sendKeys("fabio
 console.log('inserindo nome....');
 
 driver.findElement(By.id("MemberLoginForm_LoginForm_qPassword")).clear();
-driver.findElement(By.id("MemberLoginForm_LoginForm_qPassword")).sendKeys("Testesenha");
+driver.findElement(By.id("MemberLoginForm_LoginForm_qPassword")).sendKeys("Fenrirlokison123");
 console.log('inserindo senha....');
 driver.findElement(By.className("qlik-submit")).click();
 var wtg = setInterval(function () {
@@ -32,17 +35,22 @@ setTimeout(function(){
     var robot = require("robotjs");
     robot.setMouseDelay(6);
     robot.moveMouse(400, 400);
+    
     setTimeout(function(){
         robot.setKeyboardDelay(80);
         robot.typeString("C:\\Users\\fabio\\Documents\\Qlik\\Sense\\Apps\\ENGECO_NEW.qvf");
         robot.keyTap("enter");
     },
-        10000);
+    10000);
     setTimeout(function () {
-        driver.findElement(By.id("done-upload")).click();
+             driver.findElement(By.id("done-upload")).click();
     },
-        60000);
+    60000);
 
+    setTimeout(function(){
+        robot.moveMouse(400, 400);
+
+    }, 70000 );
 
     
 }, 60000);
@@ -51,8 +59,20 @@ setTimeout(function(){
 
 
 
-/*
-var robot = require("robotjs");
-robot.setMouseDelay(6);
-robot.moveMouse(400, 400);
 */
+
+var robot = require("robotjs");
+
+
+    setTimeout(function () {
+        robot.setMouseDelay(6);
+        robot.moveMouse(400, 300);
+        robot.mouseClick('right');
+        robot.moveMouse(480, 300);
+     // remover   robot.moveMouse(480, 400);
+        robot.moveMouse(480, 420);
+        robot.mouseClick();
+        robot.keyTap('enter');
+
+    }, 2000);
+
